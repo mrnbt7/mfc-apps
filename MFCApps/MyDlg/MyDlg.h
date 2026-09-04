308942b41d37b17b206c73e5fb4a44ef5619f550
mrnbt7 <mrnbt7@gmail.com>
2026-09-04 08:23:55 +0530

    MFC Dialog Based App  - Initial code

---
+++ MFCApps/MyDlg/MyDlg.h
@@ -0,0 +1,32 @@
+
+// MyDlg.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMyDlgApp:
+// See MyDlg.cpp for the implementation of this class
+//
+
+class CMyDlgApp : public CWinApp
+{
+public:
+	CMyDlgApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMyDlgApp theApp;
